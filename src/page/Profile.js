7e5aba025dd628f0e6eb6a66c555374e4e27875d
Ronald_D_Bezerra <rebulgame@gmail.com>
2022-04-18 22:40:29 -0300

    cria page Profile

---
+++ src/page/Profile.js
@@ -0,0 +1,54 @@
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import Header from '../components/Header';
+import { getUser } from '../services/userAPI';
+
+function Profile() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [description, setDescription] = useState('');
+  const [imageProfile, setImageProfile] = useState('');
+
+  useEffect(() => {
+      const userInfo = getUser();
+
+      setName(userInfo.name);
+
+      setEmail(userInfo.email);
+
+      setDescription(userInfo.description);
+
+      setImageProfile(userInfo.image);
+  }, []);
+
+  return (
+    <>
+
+      <Header />
+
+      <div>
+        <img src={imageProfile} />
+        <Link to="/profile/edit">
+          <button>Editar perfil</button>
+        </Link>
+      </div>
+
+      <div>
+        <h3>Name</h3>
+        <p>{name}</p>
+      </div>
+
+      <div>
+        <h3>Email</h3>
+        <p>{email}</p>
+      </div>
+
+      <div>
+        <h3>Descrição</h3>
+        <p>{description}</p>
+      </div>
+    </>
+  );
+}
+
+export default Profile;

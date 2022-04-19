13a65c5aac991fca604fbfa0d2a40f4b90994a04
Ronald_D_Bezerra <rebulgame@gmail.com>
2022-04-18 22:36:52 -0300

    adiciona rotas ao app.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import HomeTunes from './HomeTunes';
+import SearchTunes from '../src/page/SearchTunes';
+import Musics from '../src/page/Musics';
+import Favorites from '../src/page/Favorites';
+import Profile from '../src/page/Profile';
+import ProfileEdit from '../src/page/ProfileEdit';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Routes>
+        <Route path="/" element={<HomeTunes />} />
+        <Route path="/search" element={<SearchTunes />} />
+        <Route path="/album/:id" element={<Musics />} />
+        <Route path="/favorites" element={<Favorites />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/profile/edit" element={<ProfileEdit />} />
+      </Routes>
   );
 }
 

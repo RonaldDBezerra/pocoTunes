877d54c14e6f8777fa7c7a07ad58082f4818da4a
Ronald_D_Bezerra <rebulgame@gmail.com>
2022-04-18 22:40:11 -0300

    cria page Musics

---
+++ src/page/Musics.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../components/Header';
+import MusicsCard from '../components/MusicsCard';
+
+function Musics() {
+  return (
+    <>
+      <Header />
+      <MusicsCard />
+    </>
+  );
+}
+
+export default Musics;
